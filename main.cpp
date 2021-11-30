248e131c5a9c7d451c1194cf1226448149fd75e3
Test <Test@test.com>
2021-11-29 23:38:31 -0500

    Final

--- main.cpp
+++ main.cpp
@@ -12,3 +12,33 @@
 * called +, –, *, /, plus, minus, mul, and div with their obvious meanings.
 */
 
+#include "std_lib_facilities.h"	
+
+int main()
+{
+    try {
+        // Init vars
+        string op;
+        double var1 = 0;
+        double var2 = 0;
+        cout << "Enter an operation (+, -, *, /, plus, minus, mul, or div) and two operands, all separated by one space: ";
+        // Set while loop to match the expected input order
+        while (cin >> op >> var1 >> var2) {
+            //Reset answer between runs
+            double ans = 0;
+            if (op == "+" || op == "plus") ans = var1 + var2;
+            else if (op == "-" || op == "minus") ans = var1 - var2;
+            else if (op == "*" || op == "mul") ans = var1 * var2;
+            else if (op == "/" || op == "div") {
+                if (var2 == 0) error("Error, cannot divide by 0, please try again.");
+                ans = var1 / var2;
+            }
+            else error("Error, please use a valid operator (+, -, *, /, plus, minus, mul, or div)");
+            cout << "Answer: " << var1 << " " << op << " " << var2 << " = " << ans << "\n";
+            cout << "Enter an operation (+, -, *, /, plus, minus, mul, or div) and two operands, all separated by one space: ";
+        }
+        cout << "Something is wrong with the input, please try again.";
+    } catch (runtime_error e) {
+        cout << e.what() << "\n";
+    }
+}
